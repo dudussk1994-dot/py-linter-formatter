42bf55dd4794b793eb9b8b8acc0c923b655821f5
Damian <dudussk@go2.pl>
2026-01-04 13:18:45 +0100

    linter formatter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["message"],
+        "name": error["name"],
+        "source": error["source"]
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return{
+        "file_path": file_path,
+        "errors": errors,
+        "line": errors["line_number"],
+        "column": errors["column_number"],
+        "message": errors["message"],
+        "name": errors["name"],
+        "source": errors["source"]
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return {
+        "file_path": linter_report["file_path"],
+        "errors": linter_report["errors"],
+        "line": linter_report["line"],
+        "column": linter_report["column"],
+        "message": linter_report["message"],
+        "name": linter_report["name"],
+        "source": linter_report["source"]
+    }
