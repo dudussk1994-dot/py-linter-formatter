1df6f9de529018f2122302f1d351df5a388ba3bc
Damian <dudussk@go2.pl>
2026-01-05 16:19:05 +0100

    linter formatter two

--- app/main.py
+++ app/main.py
@@ -2,31 +2,22 @@ def format_linter_error(error: dict) -> dict:
     return {
         "line": error["line_number"],
         "column": error["column_number"],
-        "message": error["message"],
-        "name": error["name"],
-        "source": error["source"]
+        "message": error["text"],
+        "name": error["code"],
+        "source": error["flake8"]
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return{
-        "file_path": file_path,
-        "errors": errors,
-        "line": errors["line_number"],
-        "column": errors["column_number"],
-        "message": errors["message"],
-        "name": errors["name"],
-        "source": errors["source"]
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if len(errors) == 0 else "passed",
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return {
-        "file_path": linter_report["file_path"],
         "errors": linter_report["errors"],
-        "line": linter_report["line"],
-        "column": linter_report["column"],
-        "message": linter_report["message"],
-        "name": linter_report["name"],
-        "source": linter_report["source"]
+        "path": linter_report["path"]
     }
